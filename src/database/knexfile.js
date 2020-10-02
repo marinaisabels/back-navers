a696d1dc55a46ba771844f8764496ed920db016c
marinaisabels <marinaisabels@live.com>
2020-10-02 10:26:56 -0300

    changing database settings

--- src/database/knexfile.js
+++ src/database/knexfile.js
@@ -2,16 +2,16 @@ import path from 'path'
 
 import { DATABASE, DATABASE_TEST } from '../config'
 
-// export const development = {
-//   client: 'mysql',
-//   connection: DATABASE,
-//   migrations: {
-//     directory: path.resolve(__dirname, 'migrations')
-//   }
-// }
+export const development = {
+  client: 'pg',
+  connection: DATABASE,
+  migrations: {
+    directory: path.resolve(__dirname, 'migrations')
+  }
+}
 
 export const production = {
-  client: 'mysql',
+  client: 'pg',
   connection: DATABASE,
   migrations: {
     directory: path.resolve(__dirname, 'migrations')
@@ -19,31 +19,17 @@ export const production = {
 }
 
 export const test = {
-  client: 'mysql',
+  client: 'pg',
   connection: DATABASE_TEST,
   migrations: {
     directory: path.resolve(__dirname, 'migrations')
   }
 }
 
-
-export const development = {
-  client: 'mysql',
-  connection: {
-    host: 'ec2-18-229-236-15.sa-east-1.compute.amazonaws.com',
-    user: 'sagan_marina',
-    password: 'rWQCIrgZ6bWa',
-    database: 'sagan_marina_db'
-  },
-  migrations: {
-    directory: path.resolve(__dirname, 'migrations')
-  }
-}
-
-
 const knex = {
   development,
   production,
   test
 }
+
 export default knex
